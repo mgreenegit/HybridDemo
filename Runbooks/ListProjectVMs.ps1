4c68251addf0af1b4346618f60fedea09b736870
Michael Greene <mgreenegit@users.noreply.github.com>
2015-10-22 18:47:37 -0500

    Updated ListProjectVMs.ps1 in Azure Automation.

--- Runbooks/ListProjectVMs.ps1
+++ Runbooks/ListProjectVMs.ps1
@@ -8,9 +8,13 @@ Select-AzureSubscription -SubscriptionId $ID
 
 $VMs = get-azurevm | ? name -like *prjx*
 
+$VMPublicDNSNames = @()
+
 foreach ($VM in $VMs) {
 	$vm | select name, servicename, status
 	.\InstallWinRMCertAzureVM.ps1 -SubscriptionName 'migreene' -servicename $vm.servicename -vm $vm.name
 	$IP = $vm | Get-AzureEndpoint | % VIP
-	invoke-command -computername "$($vm.name).cloudapp.net" -ScriptBlock {get-service termservice | select pscomputername, displayname, status} -Credential $admin -usessl
+	$VMNames += "$($vm.name).cloudapp.net"
 }
+
+invoke-command -computername $VMPublicDNSNames -ScriptBlock {get-service termservice} -Credential $admin -usessl  | select pscomputername, displayname, status
