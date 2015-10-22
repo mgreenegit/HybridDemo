2a2d55ed41e823a13dec256eafde50cb3b8023bd
Michael Greene <mgreenegit@users.noreply.github.com>
2015-10-22 13:24:02 -0500

    Updated ListProjectVMs.ps1 in Azure Automation.

--- Runbooks/ListProjectVMs.ps1
+++ Runbooks/ListProjectVMs.ps1
@@ -1,5 +1,8 @@
-Add-AzureAccount -Credential (Get-AutomationPSCredential -Name 'Subscription CoAdmin')
-select-azuresubscription -subscriptionid '59f55e3c-d6f0-446b-ad86-20c35ba8abff' -default
+$Azure = Get-AutomationConnection -Name "azure"
+$Cert = Get-AutomationCertificate -Name $Azure.AutomationCertificateName
+
+Select-AzureSubscription -default -SubscriptionId $Azure.SubscriptionID
+
 get-azurevm | ? name -like *prjx*
 get-azurermpublicipaddress | ? name -like *prjx*
 #invoke-command -computername 192.168.0.112,40.122.169.77 -ScriptBlock {get-service termservice} -Credential 
